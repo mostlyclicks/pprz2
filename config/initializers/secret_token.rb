56246849bedd2a10b2e7e287f2e0c88da982e120
mostlyclicks <carlos@mostlyclicks.com>
2013-03-02 10:41:25 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pprz2::Application.config.secret_token = 'e0c586bdfdc56bbed3d3188f4a0dc5bed318334ecb130a42ef9a2352bd8a47f069f710bd6f9c400dd338cfa53df67d9ff39048b0ac4d39675f72be7302e99ab0'
